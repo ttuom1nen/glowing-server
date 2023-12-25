575d28142788507b6e4fc1fde1208d0c5388af45
ttu <ttuominen@outlook.com>
2023-12-25 23:03:46 +0200

    Install body-parser

--- index.ts
+++ index.ts
@@ -1,18 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-var cors = require('cors')
-
 import featureTogglesRouter from "./src/featureToggles/featureToggles.routes";
+import bodyParser from "body-parser";
+var cors = require("cors");
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(express());
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 app.use(featureTogglesRouter);
-
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
